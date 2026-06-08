addc5e58ee4f0444187359c388e7ab16b8aed815
jhony santos <jhonyananias5@gmail.com>
2026-06-08 18:39:41 -0300

    primeiro commit
    site joias

---
+++ script.js
@@ -0,0 +1,75 @@
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray(".animar").forEach((elemento) => {
+    gsap.from(elemento, {
+        y: 60,
+        opacity: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: elemento,
+            start: "top 70%",
+        }
+    });
+});
+
+const banners  = document.querySelectorAll('.avatar')
+
+const listaImagens = [
+    "img/images.jpeg",
+    "img/colar.webp",
+    "img/fundo.jpeg"
+];
+
+let indice = 0;
+
+setInterval(() => {
+    indice = (indice + 1) % listaImagens.length;
+
+    banners.forEach((banner) => {
+        banner.src = listaImagens[indice]
+        banner.style.border = "4px solid white";
+    });
+}, 3000);
+
+ const myObserver = new IntersectionObserver((entries) => { 
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+            
+         }else{
+            entry.target.classList.remove('show');
+         }
+ })
+  })
+
+  const elements = document.querySelectorAll('.hidden');
+
+
+elements.forEach((element) => {
+    myObserver.observe(element);
+});
+
+const botoes = document.querySelectorAll(".btn-comprar");
+
+botoes.forEach(botao => {
+    botao.addEventListener("click", () => {
+
+        const card = botao.closest(".card-brinco");
+
+        const nome = card.querySelector("h4").textContent;
+        const modelo = card.querySelector("p").textContent;
+        const preco = card.querySelector(".preco b").textContent;
+
+        const mensagem = `Olá! Tenho interesse neste produto:
+
+✨ Nome: ${nome}
+📌 Modelo: ${modelo}
+💰 Preço: ${preco}`;
+
+        const numero = "5521000000000"; // seu número
+
+        const url = `https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`;
+
+        window.open(url, "_blank");
+    });
+});
